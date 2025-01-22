231640f6cb525a1726465802bb635a7f38877ab5
dkyocn <gioconda77@daum.net>
2025-01-22 18:00:35 +0900

    myprofileProject commit

---
+++ profileProject/src/my/myprofile.java
@@ -0,0 +1,21 @@
+package my;
+
+/*
+ * 첫번째 프로그램
+ * 
+ * 프로파일
+ * 이름, 나이, 전화번호, 주소, 이메일 출력
+ */
+
+public class myprofile {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이름: 정동주");
+		System.out.println("나이: 27세");
+		System.out.println("전화번호: 010-4915-2698");
+		System.out.println("주소: 서울시 은평구 통일로 630");
+		System.out.println("이메일: gioconda77@daum.net");
+	}
+
+}
