2f0d36951cd72e501d4054cb0d523ca3fba8fc9b
dkyocn <gioconda77@daum.net>
2025-01-30 22:29:14 +0900

    operator practice

---
+++ testOperator/src/com/test/practice/Test.java
@@ -0,0 +1,26 @@
+package com.test.practice;
+
+import com.test.operator.Example;
+
+public class Test {
+	
+	public static void main (String[] a) {
+		Example ex = new Example();
+		
+		// 문제 1
+//		System.out.println("[문제 1]");
+//		ex.sample1();
+		
+		// 문제 2
+//		System.out.println("[문제 2]");
+//		ex.sample2();
+		
+		// 문제 3
+//		System.out.println("[문제 3]");
+//		ex.sample3();
+		
+		// 문제 4
+		System.out.println("[문제 4]");
+		ex.sample4();
+	}
+}
