06ce525dabd9a4a199685d18f7100d0217da2314
dkyocn <gioconda77@daum.net>
2025-02-12 14:15:01 +0900

    OOP array practice

--- objectArrayPractice1/src/member/model/dao/MemberManager.java
+++ objectArrayPractice1/src/member/model/dao/MemberManager.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 import member.model.dto.Member;
 
 public class MemberManager {
-	
+
 	private Scanner scanner = new Scanner(System.in);
-	
+
 	private int memberCount = 0;
 
 	/*
@@ -19,11 +19,11 @@ public class MemberManager {
 	public int getMemberCount() {
 		return this.memberCount;
 	}
-	
+
 	// 새 회원 등록 - 객체 배열 사용
 	public Member memberInput() {
 		System.out.println("[등록할 회원 정보 입력]");
-		
+
 		System.out.printf("아이디 입력: ");
 		String id = scanner.next();
 		System.out.printf("이름 입력: ");
@@ -36,7 +36,9 @@ public class MemberManager {
 		char gender = scanner.next().toUpperCase().charAt(0);
 		System.out.printf("나이 입력: ");
 		int age = scanner.nextInt();
-		
+
+		memberCount++;
+
 		return new Member(id, name, pwd, email, gender, age);
 
 	}
@@ -45,10 +47,11 @@ public class MemberManager {
 	public void deleteMember(Member[] members) {
 
 		System.out.printf("삭제할 회원의 아이디 입력: ");
-		int num = searchMemberId(members,scanner.next());
-		
+		int num = searchMemberId(members, scanner.next());
+
 		if (num != -1) {
 			members[num] = null;
+			memberCount--;
 		} else {
 			System.out.println("회원정보가 존재하지 않습니다.");
 		}
@@ -56,31 +59,31 @@ public class MemberManager {
 
 	// 모두 출력
 	public void printAllMember(Member[] members) {
-		for(Member member : members) {
-			if(member != null) {
+		for (Member member : members) {
+			if (member != null) {
 				System.out.println(member.memberInfo());
 			}
 		}
 
 	}
-	
+
 	// 단일 출력
-		public void printMember(Member[] members, int index) {
-			
-			if (index != -1) {
-				System.out.println(members[index].memberInfo());
-			} else {
-				System.out.println("회원정보가 존재하지 않습니다.");
-			}
+	public void printMember(Member[] members, int index) {
+
+		if (index != -1) {
+			System.out.println(members[index].memberInfo());
+		} else {
+			System.out.println("회원정보가 존재하지 않습니다.");
 		}
+	}
 
 	// 아이디 검색
 	public int searchMemberId(Member[] members, String id) {
-		
+
 		int returnNum = -1;
-		
-		for(int i = 0; i<members.length;i++) {
-			if(members[i] != null && members[i].getId().equals(id)) {
+
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] != null && members[i].getId().equals(id)) {
 				returnNum = i;
 			}
 		}
@@ -90,11 +93,11 @@ public class MemberManager {
 
 	// 이름 검색
 	public int searchMemberName(Member[] members, String name) {
-		
+
 		int returnNum = -1;
-		
-		for(int i = 0; i<members.length;i++) {
-			if(members[i] != null && members[i].getName().equals(name)) {
+
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] != null && members[i].getName().equals(name)) {
 				returnNum = i;
 			}
 		}
@@ -105,11 +108,11 @@ public class MemberManager {
 
 	// 이메일 검색
 	public int searchMemberEmail(Member[] members, String email) {
-		
+
 		int returnNum = -1;
-		
-		for(int i = 0; i<members.length;i++) {
-			if(members[i] != null && members[i].getEmail().equals(email)) {
+
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] != null && members[i].getEmail().equals(email)) {
 				returnNum = i;
 			}
 		}
@@ -118,42 +121,110 @@ public class MemberManager {
 
 	}
 
+	// id 오름차순 정렬
 	public void sortIDAsc(Member[] members) {
 
-//		Arrays.sort(members, new Comparator<Member>() {
-//			
-//			@Override
-//			public int compare(Member member, Member member2) {
-//				return  Integer.c
-//			}
-//		});
-//		
+		String[] ids = new String[memberCount];
+		int num = 0;
+		for (int i = 0; i < members.length; i++) {
+
+			if (members[i] != null) {
+				ids[num] = members[i].getId();
+				num++;
+			}
+		}
+
+		Arrays.sort(ids, (memberId, member2Id) -> memberId.compareTo(member2Id));
+
+		for (int i = 0; i < ids.length; i++) {
+			printMember(members, searchMemberId(members, ids[i]));
+		}
+
 	}
 
-	public void sortIDDes() {
-		// TODO Auto-generated method stub
-		
+	// id 내림차순 정렬
+	public void sortIDDes(Member[] members) {
+
+		String[] ids = new String[memberCount];
+		int num = 0;
+		for (int i = 0; i < members.length; i++) {
+
+			if (members[i] != null) {
+				ids[num] = members[i].getId();
+				num++;
+			}
+		}
+
+		Arrays.sort(ids, (memberId, member2Id) -> member2Id.compareTo(memberId));
+
+		for (int i = 0; i < ids.length; i++) {
+			printMember(members, searchMemberId(members, ids[i]));
+		}
+
 	}
 
-	public void sortAgeAsc() {
-		// TODO Auto-generated method stub
-		
+	// 나이 오름차순 정렬
+	public void sortAgeAsc(Member[] members) {
+
+		Arrays.sort(members, new Comparator<Member>() {
+
+			@Override
+			public int compare(Member m1, Member m2) {
+				if (m1 != null && m2 != null) {
+					return m1.getAge() - m2.getAge();
+				} else {
+					return 0;
+				}
+
+			}
+		});
+
+		printAllMember(members);
+
 	}
 
-	public void sortAgeDes() {
-		// TODO Auto-generated method stub
-		
+	// 나이 내림차순 정렬
+	public void sortAgeDes(Member[] members) {
+
+		Arrays.sort(members, new Comparator<Member>() {
+
+			@Override
+			public int compare(Member m1, Member m2) {
+				if (m1 != null && m2 != null) {
+					return m2.getAge() - m1.getAge();
+				} else {
+					return 0;
+				}
+
+			}
+		});
+
+		printAllMember(members);
 	}
 
-	public void sortGenderDes() {
-		// TODO Auto-generated method stub
-		
+	// 성별 내림차순 정렬
+	public void sortGenderDes(Member[] members) {
+
+		Arrays.sort(members, new Comparator<Member>() {
+
+			@Override
+			public int compare(Member m1, Member m2) {
+				if (m1 != null && m2 != null) {
+					if (m1.getGender() > m2.getGender()) {
+						return -1;
+					} else if (m1.getGender() == m2.getGender()) {
+						return 0;
+					} else {
+						return 1;
+					}
+				} else {
+					return 0;
+				}
+
+			}
+		});
+
+		printAllMember(members);
+
 	}
-	
-	
-	/*
-	 * 
-	 */
-	
-	
 }
--- objectArrayPractice1/src/member/view/MemberMenu.java
+++ objectArrayPractice1/src/member/view/MemberMenu.java
@@ -11,14 +11,14 @@ public class MemberMenu {
 	MemberManager mManager = new MemberManager();
 
 	public static final int SIZE = 10;
-	
+
 	Member[] members = new Member[SIZE];
 
 	public void mainMenu() {
 
 		do {
-			System.out.printf("\n최대 등록 가능한 회훤 수는 %d명 입니다.",SIZE);
-			System.out.printf("현재 등록된 회원수는 %d명 입니다.\n",mManager.getMemberCount());
+			System.out.printf("\n최대 등록 가능한 회훤 수는 %d명 입니다.", SIZE);
+			System.out.printf("\n현재 등록된 회원수는 %d명 입니다.\n", mManager.getMemberCount());
 			System.out.println("***** 회원 관리 프로그램 *****\n"
 				+ "\n"
 				+ "1. 새 회원 등록		\n"
@@ -33,9 +33,16 @@ public class MemberMenu {
 
 			switch (num) {
 				case 1:
-					for(Member member : members) {
-						if(member == null) {
-							member = mManager.memberInput();
+					//					for (Member member : members) {
+					//						if (member == null) {
+					//							member = mManager.memberInput();
+					//							break;
+					//						}
+					//					}
+
+					for (int i = 0; i < members.length; i++) {
+						if (members[i] == null) {
+							members[i] = mManager.memberInput();
 							break;
 						}
 					}
@@ -84,15 +91,15 @@ public class MemberMenu {
 			switch (num) {
 				case 1:
 					System.out.printf("검색할 아이디 입력: ");
-					mManager.searchMemberId(members,sc.next());
+					mManager.printMember(members, mManager.searchMemberId(members, sc.next()));
 					break;
 				case 2:
 					System.out.printf("검색할 이름 입력: ");
-					mManager.searchMemberName(members,sc.next());
+					mManager.printMember(members, mManager.searchMemberName(members, sc.next()));
 					break;
 				case 3:
 					System.out.printf("검색할 이메일 입력: ");
-					mManager.searchMemberEmail(members, sc.next());
+					mManager.printMember(members, mManager.searchMemberEmail(members, sc.next()));
 					break;
 				case 9:
 					System.out.printf("메인 메뉴로 화면 이동합니다. ");
@@ -107,115 +114,115 @@ public class MemberMenu {
 
 	// 회원 정보 정렬	compareto 사용
 	public void sortMenu() {
-		
+
 		do {
-			
+
 			System.out.println("***** 회원 정보 정렬 출력 메뉴 *****\n"
-					+ "	\n"
-					+ "1. 아이디 오름차순 정렬 출력\n"
-					+ "2. 아이디 내림차순 정렬 출력\n"
-					+ "3. 나이 오름차순 정렬 출력\n"
-					+ "4. 나이 내림차순 정렬 출력\n"
-					+ "5. 성별 내림차순 정렬 출력(남여순)\n"
-					+ "9. 이전 메뉴로 가기");
+				+ "	\n"
+				+ "1. 아이디 오름차순 정렬 출력\n"
+				+ "2. 아이디 내림차순 정렬 출력\n"
+				+ "3. 나이 오름차순 정렬 출력\n"
+				+ "4. 나이 내림차순 정렬 출력\n"
+				+ "5. 성별 내림차순 정렬 출력(남여순)\n"
+				+ "9. 이전 메뉴로 가기");
 			System.out.printf("메뉴 선택: ");
 			int num = sc.nextInt();
-			
-			switch(num) {
-			case 1:
-				mManager.sortIDAsc(members);
-				break;
-			case 2:
-				mManager.sortIDDes();
-				break;
-			case 3:
-				mManager.sortAgeAsc();
-				break;
-			case 4:
-				mManager.sortAgeDes();
-				break;
-			case 5:
-				mManager.sortGenderDes();
-				break;
-			case 9:
-				System.out.printf("메인 메뉴로 화면 이동합니다. ");
-				return;
-			default:
-				System.out.println("잘못된 입력입니다. 다시 입력바랍니다.");
+
+			switch (num) {
+				case 1:
+					mManager.sortIDAsc(members);
+					break;
+				case 2:
+					mManager.sortIDDes(members);
+					break;
+				case 3:
+					mManager.sortAgeAsc(members);
+					break;
+				case 4:
+					mManager.sortAgeDes(members);
+					break;
+				case 5:
+					mManager.sortGenderDes(members);
+					break;
+				case 9:
+					System.out.printf("메인 메뉴로 화면 이동합니다. ");
+					return;
+				default:
+					System.out.println("잘못된 입력입니다. 다시 입력바랍니다.");
 			}
-			
+
 		} while (true);
 	}
 
 	// 회원 정보 수정 - setter 사용
 	public void modifyMenu() {
-		
+
 		int memberId = -1;
-		
+
 		do {
-			
+
 			System.out.println("***** 회원 정보 수정 메뉴 *****\n"
-					+ "\n"
-					+ "1. 암호 변경\n"
-					+ "2. 이메일 변경\n"
-					+ "3. 나이 변경\n"
-					+ "9. 이전 메뉴로 가기");
+				+ "\n"
+				+ "1. 암호 변경\n"
+				+ "2. 이메일 변경\n"
+				+ "3. 나이 변경\n"
+				+ "9. 이전 메뉴로 가기");
 			System.out.printf("메뉴 선택: ");
 			int num = sc.nextInt();
-			
-			switch(num) {
-			case 1:
-				System.out.printf("비밀번호를 변경할 회원의 아이디 입력: ");
-				memberId = mManager.searchMemberId(members,sc.next());
-				
-				if(memberId != -1) {
-					System.out.println(members[memberId].memberInfo());
-					
-					System.out.printf("변경할 암호: ");
-					members[memberId].setPassword(sc.next());
-					System.out.println("회원의 정보가 변경되었습니다.");
-				} else {
-					System.out.println("회원정보가 존재하지 않습니다.");
-				}
-				
-				break;
-			case 2:
-				System.out.printf("이메일을 변경할 회원의 아이디 입력: ");
-				memberId = mManager.searchMemberId(members,sc.next());
-				
-				if(memberId != -1) {
-					System.out.println(members[memberId].memberInfo());
-					
-					System.out.printf("변경할 이메일: ");
-					members[memberId].setEmail(sc.next());
-					System.out.println("회원의 정보가 변경되었습니다.");
-				} else {
-					System.out.println("회원정보가 존재하지 않습니다.");
-				}
-				
-				break;
-			case 3:
-				System.out.printf("이메일을 변경할 회원의 아이디 입력: ");
-				memberId = mManager.searchMemberId(members,sc.next());
-				
-				if(memberId != -1) {
-					System.out.println(members[memberId].memberInfo());
-					
-					System.out.printf("변경할 나이: ");
-					members[memberId].setAge(sc.nextInt());
-					System.out.println("회원의 정보가 변경되었습니다.");
-				} else {
-					System.out.println("회원정보가 존재하지 않습니다.");
-				}
-				
-				break;
-			case 9:
-				System.out.printf("메인 메뉴로 화면 이동합니다. ");
-				return;
-			default:
-				System.out.println("잘못된 입력입니다. 다시 입력바랍니다.");
+
+			switch (num) {
+				case 1:
+					System.out.printf("비밀번호를 변경할 회원의 아이디 입력: ");
+					memberId = mManager.searchMemberId(members, sc.next());
+
+					if (memberId != -1) {
+						System.out.println(members[memberId].memberInfo());
+
+						System.out.printf("변경할 암호: ");
+						members[memberId].setPassword(sc.next());
+						System.out.println("회원의 정보가 변경되었습니다.");
+					} else {
+						System.out.println("회원정보가 존재하지 않습니다.");
+					}
+
+					break;
+				case 2:
+					System.out.printf("이메일을 변경할 회원의 아이디 입력: ");
+					memberId = mManager.searchMemberId(members, sc.next());
+
+					if (memberId != -1) {
+						System.out.println(members[memberId].memberInfo());
+
+						System.out.printf("변경할 이메일: ");
+						members[memberId].setEmail(sc.next());
+						System.out.println("회원의 정보가 변경되었습니다.");
+					} else {
+						System.out.println("회원정보가 존재하지 않습니다.");
+					}
+
+					break;
+				case 3:
+					System.out.printf("이메일을 변경할 회원의 아이디 입력: ");
+					memberId = mManager.searchMemberId(members, sc.next());
+
+					if (memberId != -1) {
+						System.out.println(members[memberId].memberInfo());
+
+						System.out.printf("변경할 나이: ");
+						members[memberId].setAge(sc.nextInt());
+						System.out.println("회원의 정보가 변경되었습니다.");
+					} else {
+						System.out.println("회원정보가 존재하지 않습니다.");
+					}
+
+					break;
+				case 9:
+					System.out.printf("메인 메뉴로 화면 이동합니다. ");
+					return;
+				default:
+					System.out.println("잘못된 입력입니다. 다시 입력바랍니다.");
 			}
-			
+
 		} while (true);
 	}
 
