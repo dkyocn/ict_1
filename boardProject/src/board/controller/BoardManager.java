620910434c01f7e104c11ad915ffcb002e99e3df
dkyocn <gioconda77@daum.net>
2025-03-20 18:19:09 +0900

    board homework

---
+++ boardProject/src/board/controller/BoardManager.java
@@ -0,0 +1,41 @@
+package board.controller;
+
+public class BoardManager {
+
+	public void writeBoard() {
+
+	}
+
+	public void displayAllList() {
+
+	}
+
+	public void displayBoard() {
+
+	}
+
+	public void modifyTitle() {
+
+	}
+
+	public void modifyContent() {
+
+	}
+
+	public void deleteBoard() {
+
+	}
+
+	public void searchBoard() {
+
+	}
+
+	public void saveListFile() {
+
+	}
+
+	public void sortList(int item, boolean isDesc) {
+
+	}
+
+}
